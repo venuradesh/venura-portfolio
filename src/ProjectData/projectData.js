c5972eb369b9fb996ce9e2028395c4f21fce0d69
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2023-04-09 11:47:01 +0530

    projects section responsiveness added

--- src/ProjectData/projectData.js
+++ src/ProjectData/projectData.js
@@ -7,7 +7,7 @@ const projectData = [
     title: "Janiro Moving International (Pvt) Ltd",
     desc: "It is a company that sends employees to New Zealand for working. Mainly they are focusing on medical side. So the client wants me to develop the Full stack website from the UI design to the end.",
     cover: Janiro,
-    skills: ["Adobe XD", "ReactJs", "styled-components", "EmailJs", "NodeJs", "ExpressJs", "Firebase"],
+    skills: ["Adobe XD", "ReactJs", "EmailJs", "NodeJs", "ExpressJs", "Firebase", "styled-components"],
     links: {
       linkedin: "https://www.linkedin.com/posts/venurawarnasooriya_design-design-reactjs-activity-7007622254188511233-x2Hs?utm_source=share&utm_medium=member_desktop",
       github: "https://github.com/venuradesh/jeniro-international-holdings.git",
@@ -18,7 +18,7 @@ const projectData = [
     title: "Sri EventZ Web",
     desc: "Sri EventZ is an event planning platform which integrates every service related with any event. For an example if you want to plan your wedding you can find photographers, makeup artists, wedding cake designers, wedding card creators, etc and you can chat with them and make a booking as needed. from this platform. This was made for the HackX2021 Hackathon Finals.",
     cover: SriEventz,
-    skills: ["Adobe XD", "ReactJs", "styled-components", "Redux", "GSAP", "NodeJs", "ExpressJs", "Firebase"],
+    skills: ["Adobe XD", "ReactJs", "Redux", "NodeJs", "ExpressJs", "Firebase", "GSAP", "styled-components"],
     links: {
       linkedin: "",
       github: "https://github.com/venuradesh/sri-eventz-web.git",
@@ -29,7 +29,7 @@ const projectData = [
     title: "RK Learning Dashboard",
     desc: "Client wants to add his courses to the database once login to the system and need to show them in the home page. Also he needs to mark if any user pays him to buy those courses. He asked me to keep the things simple and gave me 2 days to deliver.",
     cover: RkLearning,
-    skills: ["Adobe XD", "ReactJs", "styled-components", "firebase"],
+    skills: ["Adobe XD", "ReactJs", "firebase", "styled-components"],
     links: {
       linkedin: "https://www.linkedin.com/posts/venurawarnasooriya_reactjs-react-project-activity-6954649598766321664-X8oe?utm_source=share&utm_medium=member_desktop",
       github: "https://github.com/venuradesh/RK-Learning",
